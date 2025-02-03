5d70ced8767f7936545eb87ee666b43a6c61b53f
VENOM <llDEVILVENOMll@gmail.com>
2025-02-03 13:45:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,10 +19,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002211725604"))
 
 # Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", None))
+OWNER_ID = int(getenv("OWNER_ID", "7537160797"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/nobita_all_bot")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+OsAgHgPQwyU4Zjg1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/FEELING_SMILEY")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/FRIENSHIP_CLUB_GROUP")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -81,12 +81,12 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/6ncc2n.jpg"
+    "START_IMG_URL", "https://envs.sh/rfF.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/6ncc2n.jpg"
+    "PING_IMG_URL", "https://envs.sh/sNF.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/6ncc2n.jpg"
+PLAYLIST_IMG_URL = "https://envs.sh/rgV.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/6ncc2n.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/6ncc2n.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/6ncc2n.jpg"
